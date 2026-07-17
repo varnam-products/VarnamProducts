721abede9883d1bfdcaef4aae5e66b52ea593ce5
varnam-products <varnam.mda@gmail.com>
2026-07-17 18:44:40 +0530

    Delete server directory

--- server/utils/generateOrderNumber.js
+++
@@ -1,7 +0,0 @@
-import crypto from 'crypto';
-
-export const generateOrderNumber = () => {
-  const date = new Date().toISOString().slice(0, 10).replace(/-/g, '');
-  const randomSuffix = crypto.randomBytes(4).toString('hex').toUpperCase();
-  return `ORD-${date}-${randomSuffix}`;
-};
