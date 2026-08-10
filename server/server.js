e6575b52d62ce6ecfb97a82459e0fd9bb5d24157
varnam-products <varnam.mda@gmail.com>
2026-08-10 18:04:41 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -36,7 +36,7 @@ app.set('trust proxy', 1);
 
 app.use(helmet());
 
-const allowedOrigins = [process.env.CLIENT_URL, 'http://localhost:3000', 'https://varnam-products-frontend.vercel.app'].filter(Boolean);
+const allowedOrigins = [process.env.CLIENT_URL, 'http://localhost:3000', 'https://varnam-products-frontend.vercel.app', 'https://varnam-products-umber.vercel.app'].filter(Boolean);
 app.use(
   cors({
     origin: function (origin, callback) {
